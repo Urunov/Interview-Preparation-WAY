f3cb907812f0c0b80d8c455828b2f57b4963ffe1
Abbos Fayziboev <abbosfayziboev@gmail.com>
2023-05-15 13:34:02 +0500

    Delete AdapterServices directory

--- AdapterServices/BurgerOrder.ts
+++
@@ -1,7 +0,0 @@
-import { Burger } from "../interfaces/Burger";
-
-export class BurgerOrder implements Burger{
-    orderBurger(quantity: number): void {
-        console.log(`Order burger with quantity ${quantity}`);
-    }
-}
--- AdapterServices/FrieOrder.ts
+++
@@ -1,7 +0,0 @@
-import { Fries } from "../interfaces/Frie";
-
-export class FrieOrder implements Fries{
-    orderFries(fries: number): void {
-        console.log(`Order burger with quantity ${fries}`);
-    }
-}
